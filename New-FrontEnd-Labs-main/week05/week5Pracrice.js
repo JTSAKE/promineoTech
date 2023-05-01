3a504f548bf3f140f02801694169de9204e76ab7
Jesus Ruelas <jaruelas26@gmail.com>
2023-05-01 13:21:02 -0600

    Completed week5 code along Menu App

--- New-FrontEnd-Labs-main/week05/week5Pracrice.js
+++ New-FrontEnd-Labs-main/week05/week5Pracrice.js
@@ -1,14 +1,139 @@
-class Student {
-    constructor(firstName, lastName, phoneNumber, grade) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.grade = grade;
+class Player {
+    constructor (name, position) {
+        this.name = name;
+        this.position = position;
     }
-
-    introduce() {
-        console.log(`${this.firstName} ${this.lastName} can be reached at ${this.phoneNumber}`)
+    describe() {
+        return `${this.name} plays ${this.position}.`;
     }
 }
 
-let student1 = new Student('Bob', 'Smith', '2084470489', 'A')
+class Team {
+    constructor(name) {
+        this.name = name;
+        this.players = [];
+    }
+
+    addPlayer(player) {
+        if (player instanceof Player) {
+            this.players.push(player);
+        } else {
+            throw new Error(`You can only add an instance of a Player. Argument is not a player: ${player}`)
+        }
+    }
+
+    describe() {
+        return `${this.name} has ${this.players.length} players.`
+    }
+}
+
+class Menu {
+    constructor() {
+        this.teams = [];
+        this.selectedTeam = null;
+    }
+
+    start() {
+        let selection = this.showMainMenuOptions();
+
+        while (selection != 0) {
+            switch (selection) {
+                case '1': 
+                    this.createTeam();
+                    break;
+                case '2':
+                    this.viewTeam();
+                    break;
+                case '3':
+                    this.deleteTeam();
+                    break;
+                case '4':
+                    this.displayTeams();
+                    break;
+                default:
+                    selection = 0;
+            }
+            selection = this.showMainMenuOptions();
+        }
+
+        alert('Goodbye!');
+    }
+
+    showMainMenuOptions() {
+        return prompt(`
+            0) Exit
+            1) Create New Team
+            2) View Team
+            3) Delete Team
+            4) Display All Teams
+        `);
+    }
+
+    showTeamMenuOptions(teamInfo) {
+        return prompt(`
+        0) Back
+        1) Create Player
+        2) Delete Player
+        --------------------
+        ${teamInfo}
+        `);
+    }
+
+    displayTeams() {
+        let teamString = '';
+        for (let i = 0; i < this.teams.length; i++) {
+            teamString += i + ') ' + this.teams[i].name + '\n';
+        }
+        alert(teamString);
+    }
+
+    createTeam() {
+        let name = prompt('Enter name for new team.');
+        this.teams.push(new Team(name));
+    }
+
+    viewTeam() {
+        let index = prompt('Enter the index of team you wish to view.');
+        if (index > -1 && index < this.teams.length) {
+            this.selectedTeam = this.teams[index];
+            let description = 'Team Name: ' + this.selectedTeam.name + '\n';
+            
+            for (let i = 0; i < this.selectedTeam.players.length; i++) {
+                description += i + ') ' + this.selectedTeam.players[i].name + 
+                ' - ' + this.selectedTeam.players[i].position + '\n';
+            }
+
+            let selection = this.showTeamMenuOptions(description);
+            switch (selection) {
+                case '1':
+                    this.createPlayer();
+                    break;
+                case '2':
+                    this.deletePlayer();
+            }
+        }
+    }
+
+    deleteTeam() {
+        let index = prompt('Enter the index of the team you wish to delete:');
+        if (index > -1 && index < this.teams.length) {
+            this.teams.splice(index, 1);
+        }
+    }
+
+    createPlayer() {
+        let name = prompt('Enter Name for New Player.');
+        let position = prompt('Enter Position for New Player.')
+        this.selectedTeam.players.push(new Player(name, position));
+    }
+
+    deletePlayer() {
+        let index = prompt('Enter the Index of the Player you wish to Delete:');
+        if (index > -1 && index < this.selectedTeam.players.length) {
+            this.selectedTeam.players.splice(index, 1)
+        }
+    }
+}
+
+let menu = new Menu();
+menu.start();
