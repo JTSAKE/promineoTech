e341d033ee2824d9636be80541e45d9fb9f11c53
Jesus Ruelas <jaruelas26@gmail.com>
2023-06-10 11:17:51 -0600

    Added responsiveness to week 10 coding assignment

--- Coding Assingments/Week10/src/app.js
+++ Coding Assingments/Week10/src/app.js
@@ -15,48 +15,37 @@ form.addEventListener('submit', function(event){
 
     // Extracting Plane Ticket Input Value
     let planeTicketInput = document.getElementById('planeTicketInput');
-    let planeTicketInputValue = planeTicketInput.value
+    let planeTicketInputValue = planeTicketInput.value;
 
-    let newRow = document.createElement('tr')
+    // Creates new Table Row
+    let newRow = document.createElement('tr');
 
-    let numberCell = document.createElement('td')
-    let cityCell = document.createElement('td')
-    let countryCell = document.createElement('td')
-    let planeCell = document.createElement('td')
+    //Creating new Cells for each of our inputs please the automatic numbering of rows
+    let numberCell = document.createElement('td');
+    let cityCell = document.createElement('td');
+    let countryCell = document.createElement('td');
+    let planeCell = document.createElement('td');
 
+    //Assigns the input values from the form to the newly created cells
     numberCell.textContent = rowNumber;
     cityCell.textContent = cityInputValue;
     countryCell.textContent = countryInputValue;
     planeCell.textContent = `$${planeTicketInputValue}`;
 
+    //Appends cells with input information to the new row
     newRow.appendChild(numberCell);
     newRow.appendChild(cityCell);
     newRow.appendChild(countryCell);
     newRow.appendChild(planeCell);
 
+    //Adds new rows with cells to the exisiting table
     table.appendChild(newRow);
 
+    //Increases row number for each row
     rowNumber += 1;
 
+    //Resent form inputs 
     cityInput.value = '';
     countryInput.value = '';
     planeTicketInput.value = '';
 });
-
-// const table = document.getElementById('#tableBody')
-
-// function addHTML(cityValue, countryValue, ticketValue) {
-//     let newRow = document.createElement('tr')
-
-//     let cityCell = document.createElement('td')
-//     let countryCell = document.createElement('td')
-//     let planeCell = document.createElement('td')
-
-//     newRow.appendChild(cityCell);
-//     newRow.appendChild(countryCell);
-//     newRow.appendChild(planeCell);
-
-//     table.appendChild(newRow);
-// }
-
-// addHTML('Sacramento', 'US', '9.99');
