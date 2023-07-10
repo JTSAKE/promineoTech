82fae399dbc61b497f765b8a2e189b59917b7e43
Jesus Ruelas <jaruelas26@gmail.com>
2023-07-09 18:19:02 -0600

    Began coding assignment for week 14

---
+++ Coding Assingments/Week14/movie-app/components/movie.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Movie extends React.Component {
+    
+    render() {
+         return (
+            <div className='col-3'>
+                <h2 className='Display-4'></h2>
+            </div>
+         );
+    }
+}
