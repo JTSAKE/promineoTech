0a3b1ff752afb46165e6fe6c9c8f18b5be118d58
Jesus Ruelas <jaruelas26@gmail.com>
2023-07-31 13:47:18 -0600

    Completed Final Project v1

---
+++ Coding Assingments/Week16/final-project/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './components/Home';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Add from './components/Add';
+import Edit from './components/Edit';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/create' element={<Add/>}/>
+          <Route path='/edit' element={<Edit/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App
---
+++ Coding Assingments/Week16/final-project/src/components/Employees.jsx
@@ -0,0 +1,29 @@
+const Employees = [
+    {
+        id: '1',
+        Name: 'Elon Musk',
+        Age: '30'
+    },
+    {
+        id: '2',
+        Name: 'Jeff Bazos',
+        Age: '35'
+    },
+    {
+        id: '3',
+        Name: 'Tim Cook',
+        Age: '41'
+    },
+    {
+        id: '4',
+        Name: 'Bob Smith',
+        Age: '87'
+    },
+    {
+        id: '5',
+        Name: 'Frank Ocean',
+        Age: '30'
+    },
+]
+
+export default Employees;
